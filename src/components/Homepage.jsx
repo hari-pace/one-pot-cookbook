a3c2ae4c4db753dfa374b74b197a331c003655fa
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-09-18 13:00:12 +0200

    Set back the homepage changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -36,17 +36,24 @@ const Homepage = ({ recipes, setRecipes, searchBar }) => {
     <>
       <h1 className="homepage-heading">One-pot Recipes</h1>
       <div className="homepage-container">
-        {recipes &&
-          recipes.map((item) => (
-            <div key={item.fields.name} className="homepage-img-container">
-              <Link to={`/${item.fields.category}`}>
-                <img
-                  src={item.fields.image.fields.file.url}
-                  alt={item.fields.image.fields.file.title}
-                />
-              </Link>
-            </div>
-          ))}
+        <div className="homepage-img-container">
+          <Link to="/meat">
+            <h2>Meat recipes</h2>
+            <img className="homepage-img" src="" alt="meat-img" />
+          </Link>
+        </div>
+        <div className="homepage-img-container">
+          <Link to="/fish">
+            <h2>Fish recipes</h2>
+            <img className="homepage-img" src="" alt="fish-img" />
+          </Link>
+        </div>
+        <div className="homepage-img-container">
+          <Link to="/vegan">
+            <h2>Vegan recipes</h2>
+            <img className="homepage-img" src="" alt="vegan-img" />
+          </Link>
+        </div>
       </div>
     </>
   );
