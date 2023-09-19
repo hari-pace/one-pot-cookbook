a2ea580c903f42db52f94dc04986f94016fa9428
Renata Siquieroli <resiquieroli@gmail.com>
2023-09-19 12:17:05 +0200

    banners and font

---
+++ src/components/Banner2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <div className="banner2">
+      <h2>
+        Less cleaning, more exploring flavors – that's what we call a win-win
+        situation!
+      </h2>
+    </div>
+  );
+}
